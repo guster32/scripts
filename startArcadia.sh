7d1e85a94aa72e8a775dd27cec74b98de0792dee
Gustavo Branco <guster32@gmail.com>
2022-04-24 19:15:31 -0700

    Update launch command to use generic link names rather than file names

--- startArcadia.sh
+++ startArcadia.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-qemu-system-x86_64 \
-	-cpu IvyBridge -machine q35 -smp 4 \
-	-kernel $HOME/qemux86-64/bzImage--5.14.21+git0+f9e349e174_9d5572038e-r0-qemux86-64-20220416063356.bin \
-	--append "root=/dev/sda oprofile.timer=1 tsc=reliable no_timer_check rcupdate.rcu_expedited=1" \
-	-drive file=$HOME/qemux86-64/core-image-arcadia-qemux86-64-20220416095151.rootfs.ext4,index=0,media=disk,format=raw
-	-device AC97 -m 8G -vga virtio -usb -device usb-tablet -display default
+qemu-system-x86_64 --enable-kvm \
+        -cpu IvyBridge -machine type=pc,accel=kvm -smp 4 \
+        -kernel $HOME/qemux86-64/bzImage-qemux86-64.bin \
+        --append "root=/dev/sda oprofile.timer=1 tsc=reliable no_timer_check rcupdate.rcu_expedited=1" \
+        -drive file=$HOME/qemux86-64/core-image-arcadia-qemux86-64.ext4,index=0,media=disk,format=raw \
+        -device AC97 -m 8G -vga virtio -usb -device usb-tablet -display sdl,gl=on
+
