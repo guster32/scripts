2145924ab5fd40bab3a3835b30caa6c2fa75780e
Gustavo Branco <guster32@gmail.com>
2022-08-25 22:10:20 -0700

    Use stdio for serial port when launching Qemu

--- host/launchQemuDev.sh
+++ host/launchQemuDev.sh
@@ -4,7 +4,7 @@ qemu-system-x86_64 --enable-kvm \
         -cpu IvyBridge \
         -machine type=pc,accel=kvm \
         -smp 4 \
-        -kernel $HOME/qemux86-64/bzImage-qemux86-64.bin --append "root=/dev/sda oprofile.timer=1 tsc=reliable no_timer_check rcupdate.rcu_expedited=1" \
+        -kernel $HOME/qemux86-64/bzImage-qemux86-64.bin --append "root=/dev/sda console=ttys0 oprofile.timer=1 tsc=reliable no_timer_check rcupdate.rcu_expedited=1" \
         -drive file=$HOME/qemux86-64/core-image-arcadia-dev-qemux86-64.ext4,index=0,media=disk,format=raw \
         -device AC97 \
         -m 8G \
@@ -12,6 +12,8 @@ qemu-system-x86_64 --enable-kvm \
         -usb \
         -device usb-tablet \
         -display sdl,gl=on \
+        -parallel none \
+        -serial stdio \
         -net user,hostfwd=tcp::10022-:22 \
         -net nic 
 
