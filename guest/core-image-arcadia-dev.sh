5ce8fb64c139bb83e0f144bb1e89d100eef16b1a
Gustavo Branco <guster32@gmail.com>
2022-08-28 17:34:23 -0700

    Update scripts to clean all for dev image

--- guest/core-image-arcadia-dev.sh
+++ guest/core-image-arcadia-dev.sh
@@ -7,5 +7,6 @@ popd
 
 cd /home/builduser/poky
 source oe-init-build-env
-bitbake -c clean core-image-arcadia-dev
+bitbake -c cleanall glplay
+bitbake -c cleanall core-image-arcadia-dev
 bitbake core-image-arcadia-dev
