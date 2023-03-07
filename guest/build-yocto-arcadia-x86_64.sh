fc59d2fe1cd635a37f67c4987e83186c9a39503a
Gustavo Branco <guster32@gmail.com>
2023-03-06 22:12:51 -0800

    Fix command to fetch all packages on build yocto arcadia container

--- guest/build-yocto-arcadia-x86_64.sh
+++ guest/build-yocto-arcadia-x86_64.sh
@@ -4,4 +4,4 @@ cd /home/builduser/poky
 source oe-init-build-env
 cp /home/builduser/mnt/qemux86_64_bblayers.conf /home/builduser/poky/build/conf/bblayers.conf
 cp /home/builduser/mnt/qemux86_64_local.conf /home/builduser/poky/build/conf/local.conf
-bitbake -c fetchall core-image-arcadia-dev
+bitbake core-image-arcadia-dev --runall=fetch
