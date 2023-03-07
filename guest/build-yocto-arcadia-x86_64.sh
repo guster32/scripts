c1b7d893e9e50a53321ce6c071b8152fc7e268de
Gustavo Branco <guster32@gmail.com>
2023-03-07 06:51:55 -0800

    We should build arcadia once

--- guest/build-yocto-arcadia-x86_64.sh
+++ guest/build-yocto-arcadia-x86_64.sh
@@ -5,3 +5,4 @@ source oe-init-build-env
 cp /home/builduser/mnt/qemux86_64_bblayers.conf /home/builduser/poky/build/conf/bblayers.conf
 cp /home/builduser/mnt/qemux86_64_local.conf /home/builduser/poky/build/conf/local.conf
 bitbake core-image-arcadia-dev --runall=fetch
+bitbake core-image-arcadia-dev
