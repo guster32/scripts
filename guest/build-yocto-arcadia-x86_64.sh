0cb366806d230cf4f97a0dd52737d6820fe45f71
Gustavo Branco <guster32@gmail.com>
2023-03-07 22:51:20 -0800

    Update build arcadia script

--- guest/build-yocto-arcadia-x86_64.sh
+++ guest/build-yocto-arcadia-x86_64.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git clone -b kirkstone --depth=1 https://github.com/guster32/meta-arcadia.git /home/builduser/meta-arcadia
 cd /home/builduser/poky
 source oe-init-build-env
 cp /home/builduser/mnt/qemux86_64_bblayers.conf /home/builduser/poky/build/conf/bblayers.conf
