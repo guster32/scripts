397eef948cf6cd585a94675e494484253737ba55
Gustavo Branco <guster32@gmail.com>
2023-03-08 23:33:34 -0800

    Fix issue with meta-rust

--- guest/createArcadiaContainer-x86_64.sh
+++ guest/createArcadiaContainer-x86_64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone -b kirkstone --depth=1 https://github.com/guster32/meta-arcadia.git /home/builduser/meta-arcadia
-git clone -b master --depth=1 https://github.com/meta-rust/meta-rust.git /home/${user}/meta-rust
+git clone -b master --depth=1 https://github.com/meta-rust/meta-rust.git /home/builduser/meta-rust
 cd /home/builduser/poky
 source oe-init-build-env
 cp /home/builduser/mnt/qemux86_64_bblayers.conf /home/builduser/poky/build/conf/bblayers.conf
